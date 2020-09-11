1013bc9d6345b037e03fe57a73101b946fb29862
Nanda Hibatullah <nandahibatullah@users.noreply.github.com>
2020-09-11 16:18:47 +1200

    serverless rewrite (#12)
    * remove express + eb code
    
    * refactor app to run serverless

---
+++ app/handler.ts
@@ -0,0 +1,12 @@
+// modern module syntax
+export async function hello(event, _context, callback) {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+}
